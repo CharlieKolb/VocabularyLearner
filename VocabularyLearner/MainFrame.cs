58d97bc8b7d5b27ee77dd3351e9745984b08d088
Charlie <charlie.kolb@outlook.de>
2017-01-15 11:19:13 +0100

    V0.11: - Removed unused files - Added Language variation option

--- VocabularyLearner/MainFrame.cs
+++ VocabularyLearner/MainFrame.cs
@@ -35,9 +35,13 @@ namespace VocabularyLearner
 
         private bool AtoB = true;
 
+        private mode_ mode = mode_.AtoB;    //Directs the translation direction, A->B, B->A or random
+
         private state_ state = state_.waitingForSolution;
 
         private TextBox inputBox = new TextBox();
+        private ComboBox shuffleBox = new ComboBox();
+
 
         private Graphics formGraphics;
 
@@ -54,12 +58,13 @@ namespace VocabularyLearner
             SetBounds(300, 400, 1200, 600);
             formGraphics = CreateGraphics();
             
+            //Adds the TextBox for the User Input
             inputBox.SetBounds(230, 200, 200, 30);
             Controls.Add(inputBox);
             inputBox.KeyPress += Box_KeyPress;
             BringToFront();
 
-
+            //Loads the items read into the program by the FileHandler
             items = (new FileHandler()).getItemList();
             Debug.WriteLine(items.Count);
             foreach (Item item in items)
@@ -67,6 +72,8 @@ namespace VocabularyLearner
                 Debug.WriteLine(item.ToString());
             }
             current = items.getNext();
+
+            //Initialize the different drawing subjects
             drawables[Drawable.headline] = new Drawable("Translator-Test Press Enter to proceed", new Point(10, 10));
             drawables[Drawable.textOne] = new Drawable("Please translate: ", new Point(70, 170));
             drawables[Drawable.toBeTranslated] = new Drawable(current.getToBeTranslated(AtoB), new Point(227, 170));
@@ -77,12 +84,32 @@ namespace VocabularyLearner
             drawables[Drawable.result] = new Drawable(new Point(230, 300));
 
 
+            //Dropdown menu to select the Direction of the questions
+            shuffleBox.SetBounds(600, 100, 70, 10);
+            shuffleBox.DropDownStyle = ComboBoxStyle.DropDownList;
+            shuffleBox.Name = "Direction";
+            shuffleBox.DataSource = new string[]{"A -> B", "B -> A", "Random"};
+            shuffleBox.DropDownClosed += ShuffleBox_DropDownClosed;
+            Controls.Add(shuffleBox);
+           
+
             Application.Run(this);
+        }
+
+        private void ShuffleBox_DropDownClosed(object sender, EventArgs e)
+        {
+            if (shuffleBox.SelectedIndex == 0) mode = mode_.AtoB;//A->B
+
+            else if (shuffleBox.SelectedIndex == 1) mode = mode_.BtoA;//A->B
+
+            else if (shuffleBox.SelectedIndex == 2) mode = mode_.RtoR;//A->B
+           
         }
 
         private Item getRandomItem() {
             return items.ElementAt(random.Next(0, items.Count - 1));
         }
+        
 
         private void Box_KeyPress(object sender, KeyPressEventArgs e)
         {
@@ -101,6 +128,11 @@ namespace VocabularyLearner
                         break;
                     //User pushed continue -> Generate new word
                     case state_.waitingForContinue:
+                        switch (mode) {     //Set bool AtoB according to selected mode
+                            case mode_.AtoB: AtoB = true; break;
+                            case mode_.BtoA: AtoB = false; break;
+                            case mode_.RtoR: AtoB = random.Next(0, 2)==1 ? true : false; break; //Randomly true or false
+                        }
                         current = items.getNext();
                         drawables[Drawable.result].setDraw(false);
                         drawables[Drawable.solution].setDraw(false);
